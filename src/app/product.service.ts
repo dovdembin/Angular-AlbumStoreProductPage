2f4563dad4de5b944cc2d231b1870eb43b48751c
Dov_Dembin <Dov.Dembin@Dell.com>
2022-08-10 01:10:41 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,8 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  _albumUrl = "../assets/album.json"
+
+  constructor(private _http: Http) { }
 
 }
