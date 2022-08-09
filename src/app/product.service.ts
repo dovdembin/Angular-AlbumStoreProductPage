e6da6a091fa0e6aa186e071d79216cdf5c87bbbe
Dov_Dembin <Dov.Dembin@Dell.com>
2022-08-10 01:40:00 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,8 +9,8 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((Response) =>
-    Response.json());
+    return this._http.get(this._albumUrl).map((response) =>
+    response.json());
   }
 
 }
