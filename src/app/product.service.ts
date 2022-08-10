5afe54a5771ca805a7c0bd18a05990f635a7376a
Dov_Dembin <Dov.Dembin@Dell.com>
2022-08-10 10:44:11 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Album } from './album';
+
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
 @Injectable()
 export class ProductService {
 
@@ -9,8 +12,8 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) =>
-    response.json());
+    return Observable<Album>(this._http.get(this._albumUrl).map((response) =>
+    response.json()));
   }
 
 }
