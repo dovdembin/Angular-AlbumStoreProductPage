f10290f7650b31335493bf981edc6d7fbe71639d
Dov_Dembin <Dov.Dembin@Dell.com>
2022-08-10 11:54:13 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,7 +18,7 @@ export class ProductService {
     <Album>response.json());
   }
 
-  getProducts() {
-      return this._http.get(this._productsUrl)
+  getProducts(): Observable<Product[]> {
+      return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
   }
 }
